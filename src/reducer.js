83428f8334f11125b16c79e5fa073e86f9d4e248
unknown <tufailmirr@gmail.com>
2020-11-15 15:50:25 +0530

    did some css with scrollbars and commented the token in redcer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists : [],
     playing : false,
     item : null,
-    token : "BQAZQErPO0tXqPwQuOg5n8bK5u8ZF-tCQK59BnVDTJHp3eP4IoHYyyBTpCQk3HdIjCQF9b3lf-xunH7gKiSC1z_8jdRjLIlTOtTHsNRgTzkiLXKiUOPUbjPNQ_FrP776naQwDiveEvxhCAmhrf-fJmo5kwYvpTd_7flxt4ipByJL0L0i",
+    // token : "BQAZQErPO0tXqPwQuOg5n8bK5u8ZF-tCQK59BnVDTJHp3eP4IoHYyyBTpCQk3HdIjCQF9b3lf-xunH7gKiSC1z_8jdRjLIlTOtTHsNRgTzkiLXKiUOPUbjPNQ_FrP776naQwDiveEvxhCAmhrf-fJmo5kwYvpTd_7flxt4ipByJL0L0i",
     discover_daily : null
 
 };
